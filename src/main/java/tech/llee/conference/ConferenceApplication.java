bb6ec83aaa393ba28e03fe41609f242a318e4c11
Erico Murilo Gozzi <erico@llee.tech>
2021-05-14 12:17:39 -0300

    First commit

---
+++ src/main/java/tech/llee/conference/ConferenceApplication.java
@@ -0,0 +1,13 @@
+package tech.llee.conference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceApplication.class, args);
+    }
+
+}
