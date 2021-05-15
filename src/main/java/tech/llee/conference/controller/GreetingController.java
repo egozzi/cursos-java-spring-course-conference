c635bbdcc9167ea5ee1f3a15896bfbd5eb56f3d0
Erico Murilo Gozzi <erico@llee.tech>
2021-05-15 10:29:46 -0300

    Registration functionality up to unit 7 (Spring MVC View).

---
+++ src/main/java/tech/llee/conference/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package tech.llee.conference.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("greeting")
+    public String greeting (Map<String, Object> model) {
+        model.put("message", "Olá Erico");
+        return "greeting";
+    }
+}
